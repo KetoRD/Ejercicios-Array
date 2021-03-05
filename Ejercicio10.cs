3183d53e8ca9193a0b15999a7a57e15e71c68ae5
KetoRD <78993548+KetoRD@users.noreply.github.com>
2021-03-04 23:27:54 -0400

    Add files via upload

---
+++ Ejercicio10.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace programa10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Porfavor introduzca su nombre: ");
+            string Nombre = Console.ReadLine();
+            Console.WriteLine("Porfavor introduzca su edad:");
+            int Edad = Convert.ToInt32(Console.ReadLine());
+            if (Edad >= 18)
+            {
+                Console.WriteLine("Nombre: " + Nombre);
+                Console.WriteLine("Edad: " + Edad);
+                Console.WriteLine("Usted es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Nombre: " + Nombre);
+                Console.WriteLine("Edad: " + Edad);
+                Console.WriteLine("Usted no es mayor de edad");
+            }
+        }
+    }
+}
---
+++ Ejercicio7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace programa7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba un nombre: ");
+            string nombre = Console.ReadLine();
+            int a = nombre.Length;
+            int b;
+            a--;
+            for (b = 0; b <= a; b++)
+                Console.Write(nombre[b] + " ");
+        }
+    }
+}
